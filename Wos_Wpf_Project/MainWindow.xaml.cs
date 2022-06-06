949a5635fab406d98318806405a9b881dd08cc4b
k-wos <kacper.wos33@gmail.com>
2022-06-06 22:13:31 +0200

    UI

--- Wos_Wpf_Project/MainWindow.xaml.cs
+++ Wos_Wpf_Project/MainWindow.xaml.cs
@@ -24,5 +24,30 @@ namespace Wos_Wpf_Project
         {
             InitializeComponent();
         }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReadButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
