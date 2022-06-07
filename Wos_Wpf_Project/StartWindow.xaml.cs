ec47cc9065f1fac99b5dfdbc98a86a2e967bd8ad
k-wos <kacper.wos33@gmail.com>
2022-06-07 15:42:20 +0200

    StartwINDOW

---
+++ Wos_Wpf_Project/StartWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wos_Wpf_Project
+{
+    /// <summary>
+    /// Logika interakcji dla klasy StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hall_Crud(object sender, RoutedEventArgs e)
+        {
+            MainWindow hallWindow = new MainWindow();
+            hallWindow.Show();
+        }
+    }
+}
